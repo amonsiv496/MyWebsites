057a6f14e2e65efe672c790fde9b3e35670fe6dd
Anastacio Monsivais <monsivais0127@gmail.com>
2019-09-02 18:18:13 -0500

    basic-login-static-page-added

--- MyTxState/index.php
+++ MyTxState/index.php
@@ -1,11 +1,46 @@
 <!DOCTYPE html>
-<html>
+<html lang="en-US">
 <head>
 <meta charset="UTF-8">
 <title>MyTxState Login</title>
-<link rel="stylesheet" type="text/css" href="/MyTxState/css/loginPage.css">
+<link rel="stylesheet" type="text/css" href="css/loginPage.css">
 </head>
 <body>
-	<h1>Hi!</h1>
+	<section id="navigation-section">
+		<nav id="navigation-wrapper">
+			<a href="#">Login</a>
+			<a href="#">About us</a>
+			<a href="#">Register</a>
+			<a href="#">Contact</a>
+		</nav>
+	</section>
+	<section id="login-section">
+		<h1>Login</h1>
+		<div id="username-wrapper">
+			<img src="img/username-icon.svg" alt="login-icon" width="30px" height="30px">
+			<textarea id="username-text" placeholder="Username" rows="1" cols="30"></textarea>
+		</div>
+		<div id="password-wrapper">
+			<img src="img/lock-icon.svg" alt="lock-icon" width="30px" height="30px">
+			<textarea id="password-text" placeholder="Password" rows="1" cols="30"></textarea>
+		</div>
+		<div id="get-started-wrapper">
+			<a id="get-started" href="#">Get Started</a>
+		</div>
+		<p id="keep-logged-in-option">Keep logged in</p>
+		<a id="forgot-password" href="#">Forgot Password</a>
+		<div id="create-account-need-help-wrapper">
+			<a id="create-account" href="#">CREATE ACCOUNT</a>
+			<a id="need-help" href="#">Need Help?</a>
+		</div>
+	</section>
+	<footer>
+		<ul id="footer-list-wrapper">
+			<li><a id="about-us-footer" href="#">About us</a></li>
+			<li><a id="privacy-policy" href="#">Privacy Policy</a></li>
+			<li><a id="terms-of-use" href="#">Terms of Use</a></li>
+			<p id="copyright">&copy; MyTxState All Rights Reserved</p>
+		</ul>
+	</footer>
 </body>
 </html>
